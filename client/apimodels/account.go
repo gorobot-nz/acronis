ed2966e1598ea4412ef4e9c49a9b6c6004444fb5
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2023-02-22 16:41:38 +0300

    Add account model

--- client/apimodels/account.go
+++ client/apimodels/account.go
@@ -1,1 +1,48 @@
 package apimodels
+
+const (
+	AccountBillingType    = "billing"
+	AccountManagementType = "management"
+	AccountTechnicalType  = "technical"
+)
+
+const (
+	QuotaNotification             = "quota"
+	ReportsNotification           = "reports"
+	BackupErrorNotification       = "backup_error"
+	BackupWarningNotification     = "backup_warning"
+	BackupInfoNotification        = "backup_info"
+	BackupDailyReportNotification = "backup_daily_report"
+)
+
+type AccountContacts struct {
+	Email     string   `json:"email"`
+	Types     []string `json:"types"`
+	Address1  string   `json:"address1"`
+	Address2  string   `json:"address2"`
+	Country   string   `json:"country"`
+	State     string   `json:"state"`
+	ZipCode   string   `json:"zipcode"`
+	City      string   `json:"city"`
+	Phone     string   `json:"phone"`
+	FirstName string   `json:"firstname"`
+	LastName  string   `json:"lastName"`
+}
+
+type Account struct {
+	Id               string          `json:"id"`
+	Version          uint64          `json:"version"`
+	TenantId         string          `json:"tenant_id"`
+	Login            string          `json:"login"`
+	Contact          AccountContacts `json:"contact"`
+	Activated        bool            `json:"activated"`
+	Enabled          bool            `json:"enabled"`
+	TermsAccepted    bool            `json:"terms_accepted"`
+	CreatedAt        string          `json:"created_at"`
+	Language         string          `json:"language"`
+	IdpId            string          `json:"idp_id"`
+	ExternalId       string          `json:"external_id"`
+	PersonalTenantId string          `json:"personal_tenant_id"`
+	Notifications    []string        `json:"notifications"`
+	MFAStatus        string          `json:"mfa_status"`
+}
--- client/apimodels/tenant.go
+++ client/apimodels/tenant.go
@@ -7,36 +7,6 @@ const (
 	TenantUnitKind     = "unit"
 )
 
-const (
-	Bulgarian          = "bg"
-	Czech              = "cs"
-	Danish             = "da"
-	German             = "de"
-	English            = "en"
-	EnglishUs          = "en-US"
-	Spanish            = "es"
-	SpanishLatin       = "es-419"
-	Finnish            = "fi"
-	French             = "fr"
-	Indonesian         = "id"
-	Italian            = "it"
-	Hungarian          = "hu"
-	Japan              = "ja"
-	Korean             = "ko"
-	Malay              = "ms"
-	Norwegian          = "nb"
-	Dutch              = "nl"
-	Polish             = "pl"
-	Portuguese         = "pt"
-	PortugueseBrazil   = "pt-BR"
-	Russian            = "ru"
-	Serbian            = "sr"
-	Swedish            = "sv"
-	Turkish            = "tr"
-	Chinese            = "zh"
-	ChineseTraditional = "zh-TW"
-)
-
 type TenantContacts struct {
 	Address1  string `json:"address1"`
 	Address2  string `json:"address2"`
