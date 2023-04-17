0a72e5a8ffe4b1d2313b3c927cb316c8b79d5418
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2023-04-17 18:09:55 +0300

    Upd

--- pkg/client/apimodels/offering_item.go
+++ pkg/client/apimodels/offering_item.go
@@ -1,14 +1,23 @@
 package apimodels
 
+type OfferingItemEdition string
+
+const (
+	ProtectionPerGigabyte       OfferingItemEdition = "pck_per_gigabyte"
+	ProtectionPerWorkload       OfferingItemEdition = "pck_per_workload"
+	FileSyncAndSharePerGigabyte OfferingItemEdition = "fss_per_gigabyte"
+	FileSyncAndSharePerUser     OfferingItemEdition = "fss_per_user"
+)
+
 type OfferingItem struct {
-	ApplicationId   string `json:"application_id"`
-	Edition         string `json:"edition,omitempty"`
-	InfraId         string `json:"infra_id,omitempty"`
-	Locked          bool   `json:"locked"`
-	MeasurementUnit string `json:"measurement_unit"`
-	Name            string `json:"name"`
-	ItemQuota       Quota  `json:"quota"`
-	UsageName       string `json:"usage_name"`
-	Status          int    `json:"status"`
-	Type            string `json:"type"`
+	ApplicationId   string              `json:"application_id"`
+	Edition         OfferingItemEdition `json:"edition,omitempty"`
+	InfraId         string              `json:"infra_id,omitempty"`
+	Locked          bool                `json:"locked"`
+	MeasurementUnit string              `json:"measurement_unit"`
+	Name            string              `json:"name"`
+	ItemQuota       Quota               `json:"quota"`
+	UsageName       string              `json:"usage_name"`
+	Status          int                 `json:"status"`
+	Type            string              `json:"type"`
 }
