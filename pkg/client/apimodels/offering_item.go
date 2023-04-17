d329a211c60b4a1d6301b839d6c450c5b8fe4308
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2023-04-17 17:25:20 +0300

    Fix

--- pkg/client/apimodels/offering_item.go
+++ pkg/client/apimodels/offering_item.go
@@ -2,8 +2,8 @@ package apimodels
 
 type OfferingItem struct {
 	ApplicationId   string `json:"application_id"`
-	Edition         string `json:"edition"`
-	InfraId         string `json:"infra_id"`
+	Edition         string `json:"edition,omitempty"`
+	InfraId         string `json:"infra_id,omitempty"`
 	Locked          bool   `json:"locked"`
 	MeasurementUnit string `json:"measurement_unit"`
 	Name            string `json:"name"`
